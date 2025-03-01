9176d3c653c893115de3e6802903687e48bd8b14
Jose D Tutor GIT <jdlibreros@parquesoftti.com>
2025-02-28 19:58:05 -0500

    Bugs corrected

--- ejemplo.py
+++ ejemplo.py
@@ -1,7 +1,7 @@
 x = input("Hola, cual es tu nombre: ")
 y = input("Cual es tu edad: ")
 
-print(f"Hola, {x}, que tengas un lindo dia")
+print(f"Hola, {x}, que tengas un excelente dia")
 
 for i in range(1,int(y)+1):
-    print(f"Año {i}")
+    print(f"Codigo {i}")
